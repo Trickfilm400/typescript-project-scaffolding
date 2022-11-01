4cf7770072d6b9d7ff260973137655af75ac846d
Nico Wagner <info@ni-wa.de>
2022-11-01 16:26:02 +0100

    Add websocket example files

--- static/example_src/socket_io_server/index.ts
+++ static/example_src/socket_io_server/index.ts
@@ -1,1 +1,33 @@
+import * as io from 'socket.io';
+
 console.log('Start...');
+
+export interface ClientToServerEvents {
+  eventName: (params: any) => void;
+}
+
+interface ServerToClientEvents {
+  eventName: (params: any) => void;
+}
+
+class SocketServer {
+  private io: io.Server<ClientToServerEvents, ServerToClientEvents>;
+
+  constructor() {
+    this.io = new io.Server(undefined, { cors: { origin: '*' } });
+    this.io.on('connection', (socket) => this.onConnection(socket));
+  }
+
+  private async onConnection(socket: io.Socket<ClientToServerEvents, ServerToClientEvents>) {
+    console.log('New Socket Connection from', socket.handshake.address);
+    //templates
+    socket.on('eventName', (_params: any) => {});
+  }
+
+  public listen(port = 8080) {
+    this.io.listen(port);
+    console.log(`[ ${process.uptime()} ] - Socket Server listening...`);
+  }
+}
+
+new SocketServer().listen();
--- static/example_src/websocket_server/index.ts
+++ static/example_src/websocket_server/index.ts
@@ -1,1 +1,19 @@
 console.log('Start...');
+
+import { WebSocketServer } from 'ws';
+
+const wss = new WebSocketServer({
+  port: 8080,
+  host: '0.0.0.0',
+  perMessageDeflate: true,
+});
+
+wss.on('connection', function connection(ws) {
+  console.log(ws);
+
+  ws.on('message', function message(data) {
+    console.log(Date.now(), 'received: %s', data.toString());
+  });
+
+  ws.send(JSON.stringify({ server: 'say Hi' }));
+});
