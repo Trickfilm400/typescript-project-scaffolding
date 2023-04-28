358a91d4a7c9f40e64ad095f81614291d4f0bb31
Nico Wagner <info@ni-wa.de>
2023-04-28 06:12:37 +0200

    fix: update copyright in generated readme

--- src/lib/answerHandlers/createNewProjectFiles.ts
+++ src/lib/answerHandlers/createNewProjectFiles.ts
@@ -133,7 +133,7 @@ export class CreateNewProjectFiles {
     // create readme.md
     fs.writeFileSync(
       path.join(this.path, 'readme.md'),
-      `# ${this.projectName}\n\n2022\nCreated with ♥ by [typescript-project-scaffolding](https://github.com/Trickfilm400/typescript-project-scaffolding)`
+      `# ${this.projectName}\n\n&copy; 2023\n\nCreated with ♥ by [typescript-project-scaffolding](https://github.com/Trickfilm400/typescript-project-scaffolding)`
     );
     fs.copyFileSync(path.join(this.staticPath, 'gitignore'), path.join(this.path, '.gitignore'));
     //copy a tsconfig file with declarations true if it is a npm module
